ac884f42af9fe2ff1b83681f2b5ecfaa91fb5b3c
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-09-13 23:12:20 +0530

    Update home.py

--- home.py
+++ home.py
@@ -68,7 +68,7 @@ def blog():
 def file():
     placeholder.empty()
     information.empty()
-    openai.api_key = st.secrets['api_key']
+    openai.api_key = st.secrets['openAI_key']
     st.subheader("Chat with your files, powered by LlamaIndex 💬🦙")
             
     if "messages" not in st.session_state.keys(): # Initialize the chat messages history
