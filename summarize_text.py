f046d4556b841f551f136293f78c7e9d0217c68e
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-09-13 23:09:47 +0530

    Add files via upload

--- summarize_text.py
+++ summarize_text.py
@@ -1,42 +1,19 @@
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
-from heapq import nlargest
+from langchain import OpenAI
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+
+def generate_response(txt):
+    # Instantiate the LLM model
+    llm = OpenAI(temperature=0, openai_api_key="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")
+    # Split text
+    text_splitter = CharacterTextSplitter()
+    texts = text_splitter.split_text(txt)
+    # Create multiple documents
+    docs = [Document(page_content=t) for t in texts]
+    # Text summarization
+    chain = load_summarize_chain(llm, chain_type='map_reduce')
+    return chain.run(docs)
 
 
-def summarize(text, per):
-    try:
-        try:
-            nlp = spacy.load("en_core_web_md")
-        except: # If not present, we download
-            spacy.cli.download("en_core_web_md")
-            nlp = spacy.load("en_core_web_md")
-        doc = nlp(text)
-        tokens = [token.text for token in doc]
-        word_frequencies = {}
-        for word in doc:
-            if word.text.lower() not in list(STOP_WORDS):
-                if word.text.lower() not in punctuation:
-                    if word.text not in word_frequencies.keys():
-                        word_frequencies[word.text] = 1
-                    else:
-                        word_frequencies[word.text] += 1
-        max_frequency = max(word_frequencies.values())
-        for word in word_frequencies.keys():
-            word_frequencies[word] = word_frequencies[word] / max_frequency
-        sentence_tokens = [sent for sent in doc.sents]
-        sentence_scores = {}
-        for sent in sentence_tokens:
-            for word in sent:
-                if word.text.lower() in word_frequencies.keys():
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word.text.lower()]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word.text.lower()]
-        select_length = int(len(sentence_tokens) * per)
-        summary = nlargest(select_length, sentence_scores, key=sentence_scores.get)
-        final_summary = [word.text for word in summary]
-        summary = ''.join(final_summary)
-        return summary
-    except:
-        print("Unexcpected error")
+
